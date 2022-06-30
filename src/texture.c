3026327151a9cba630c326ee437d8d8052cc03bd
Guillaume HANQUER <ghanquer@e3r12p14.42.fr>
2022-06-30 15:39:58 +0200

    On securise

--- src/texture.c
+++ src/texture.c
@@ -6,7 +6,7 @@
 /*   By: ghanquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 11:10:49 by ghanquer          #+#    #+#             */
-/*   Updated: 2022/06/30 13:54:36 by ghanquer         ###   ########.fr       */
+/*   Updated: 2022/06/30 15:39:36 by ghanquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*get_texture_path(void)
 {
-	return ("texture/wall.xpm");
+	return ("textures/wall.xpm");
 }
 
 t_data	get_texture(t_info *info)
@@ -22,8 +22,9 @@ t_data	get_texture(t_info *info)
 	t_data	my_texture;
 
 	info->texture_path = get_texture_path();
-	mlx_xpm_file_to_image(info->mlx, info->texture_path, &my_texture.width, &my_texture.height);
-	my_texture.img = mlx_new_image(info->mlx, my_texture.width, my_texture.height);
+	my_texture.img = mlx_xpm_file_to_image(info->mlx, info->texture_path, &my_texture.width, &my_texture.height);
+	if (!my_texture.img)
+		return (printf("Impossible d'ouvrir la texture\n"), my_texture);
 	my_texture.addr = mlx_get_data_addr(my_texture.img, &my_texture.bits_per_pixel, &my_texture.line_length, &my_texture.endian);
 	return (my_texture);
 }
